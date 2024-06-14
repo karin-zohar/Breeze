a0768a3173c5f795c76e3c3dbaa44d1ce84b58d3
Karin Zohar <karinzohar3@gmail.com>
2024-06-14 17:42:15 +0300

    feat: add style modules, enable dark mode

--- frontend/src/root-cmp.jsx
+++ frontend/src/root-cmp.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Routes, Route } from 'react-router'
 
 export function RootCmp() {
+
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+  const toggleTheme = () => {
+    setIsDarkTheme(prevTheme => (!prevTheme))
+  }
+
   return (
-    <div className="main-layout base-layout">
+    <div className={`app-container main-layout base-layout ${isDarkTheme ? 'theme-dark': ''}`} >
+      <button onClick={toggleTheme}>Toggle theme</button>
       <main>
         <h1>
           The app is rendering correctly
