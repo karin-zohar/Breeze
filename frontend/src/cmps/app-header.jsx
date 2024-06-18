d4769e7cad4c90c69ed7d0bae88bd52a708a84c8
Karin Zohar <karinzohar3@gmail.com>
2024-06-18 18:08:30 +0300

    feat: 💄 handle app header layout

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -3,14 +3,15 @@ import { ThemeToggle } from './theme-toggle'
 export function AppHeader({ isMenuOpen, toggleMenu }) {
     return (
         <header className="app-header">
-            <ThemeToggle />
-            <button
-                className={`menu-toggle ${isMenuOpen ? 'open' : 'closed'}`}
-                onClick={toggleMenu}
-            >
-                
-            </button>
-            app header
+            <div className="logo">Breeze</div>
+            <section className="toggles">
+                <ThemeToggle />
+                <button
+                    className={`menu-toggle ${isMenuOpen ? 'open' : 'closed'}`}
+                    onClick={toggleMenu}
+                >
+                </button>
+            </section>
         </header>
     )
 }
