42fffe8aeb47dcaa4611be196c560296dc479c74
Karin Zohar <karinzohar3@gmail.com>
2024-06-25 14:36:47 +0300

    refactor: :recycle: use NavLink for navigation

--- frontend/src/cmps/app-nav.jsx
+++ frontend/src/cmps/app-nav.jsx
@@ -1,22 +1,25 @@
-import { publicRoutes } from "../constants/routes" 
+import { publicRoutes } from "../constants/routes"
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom'
 
 export function AppNav() {
-    const currentLocation = window.location.pathname
+    const location = useLocation()
     const routes = publicRoutes
-    
+
     return (
         <nav className="app-nav">
-            {routes.map(({path, title}, key) => (
-                <a
+            {routes.map(({ path, title }, key) => (
+
+                <NavLink
+                    to={path}
+                    className={(location.pathname === path) ? "active" : ""}
                     key={key}
-                    href={path}
-                    className={(currentLocation === path) ? "active" : ""}>
+                >
                     {title}
-                </a>
+                </NavLink>
 
             ))}
 
-           
+
 
         </nav>
     )
