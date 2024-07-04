c66133bf206e8b2cb547ae90d6fa887501f106df
Karin Zohar <karinzohar3@gmail.com>
2024-07-04 17:11:01 +0300

    feat: :sparkles: load projects every time project-index mounts, projects survive refresh

--- frontend/src/pages/project-index.jsx
+++ frontend/src/pages/project-index.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { ProjectList } from '../cmps/project-list.jsx'
 import { useSelector, useDispatch } from 'react-redux'
 import { selectAllProjects,loadProjects, addProject, removeProject } from '../store/projects/project.reducer.js'
@@ -6,20 +7,24 @@ import { projectService } from '../services/project.service.js'
 export default function ProjectIndex() {
     const projects = useSelector(selectAllProjects) // array
     const dispatch = useDispatch()
-
+    
+    useEffect(() => {
+        dispatch(loadProjects())
+    },[])
     const handleRemoveProject = (projectId) => {
         dispatch(removeProject(projectId))
     }
-
+    
     const handleAddProject = (projectId) => {
         let newProject = projectService.getEmptyProject()
         dispatch(addProject(newProject)) // temporary
     }
-
+    
     const projectActions = {
         remove: handleRemoveProject,
         add: handleAddProject
     }
+    
 
     return (
         <section className="project-index">
