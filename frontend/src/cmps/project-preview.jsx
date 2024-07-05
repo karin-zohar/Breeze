1f65de85382f9d07049381990e8bf2b09469723b
Karin Zohar <karinzohar3@gmail.com>
2024-07-05 11:24:03 +0300

    docs: 💡 add TODOs in comments

--- frontend/src/cmps/project-preview.jsx
+++ frontend/src/cmps/project-preview.jsx
@@ -1,12 +1,14 @@
 import { projectService } from "../services/project.service"
 
-export function ProjectPreview({project}) {
-    const {title, description} = project
+export function ProjectPreview({ project }) {
+    const { title, description } = project
     return (
         <article className="project-preview">
             <h2>{title}</h2>
             <p>{description}</p>
-            
         </article>
     )
 }
+
+//TODO: add event listener to dblclick -> contentEditable=true on the title and description
+//TOOD: save changes to db. 
--- frontend/src/pages/project-index.jsx
+++ frontend/src/pages/project-index.jsx
@@ -39,3 +39,4 @@ export default function ProjectIndex() {
     )
 }
 
+// TODO: add pagination
